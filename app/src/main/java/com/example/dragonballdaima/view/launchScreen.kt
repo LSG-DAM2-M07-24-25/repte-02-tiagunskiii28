ce27da47a8166771fb30542d54e9777097a0da3c
tiagunskii@gmail.com <tiagunskiii@gmail.com>
2024-12-13 16:27:57 +0100

    feat: menu creado

--- app/src/main/java/com/example/dragonballdaima/view/launchScreen.kt
+++ app/src/main/java/com/example/dragonballdaima/view/launchScreen.kt
@@ -1,10 +1,88 @@
 package com.example.dragonballdaima.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInWindow
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.DpOffset
+import androidx.compose.ui.unit.toSize
 import androidx.navigation.NavController
+import com.example.dragonballdaima.R
 import com.example.dragonballdaima.viewModel.viewModel
+import com.example.dragonballdaima.Routes
 
 @Composable
 fun launchScreen(myviewModel : viewModel, navController: NavController){
-
+    val iconoAplicacion = painterResource(id = R.drawable.dragonball_daima_logo)
+    val scrollState = rememberScrollState()
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ){
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .verticalScroll(scrollState),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ){
+            Image(
+                painter = iconoAplicacion,
+                contentDescription = "imagen menu",
+                modifier = Modifier
+                    .size(300.dp)
+                    .padding(30.dp),
+                contentScale = ContentScale.Fit
+            )
+            Button(
+                onClick = {
+                    navController.navigate(Routes.personatge.route)
+                } ,
+                modifier = Modifier.padding(top = 50.dp)
+            ){
+                Text(text="Entrar")
+            }
+        }
+    }
 }
